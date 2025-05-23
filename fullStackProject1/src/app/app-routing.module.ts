22c145dd47c754f19035298e6d1a4c5fc9f36356
Versha1707 <vershakumari1707@gmail.com>
2025-05-23 20:58:27 +0530

    Removed unwanted code

--- fullStackProject1/src/app/app-routing.module.ts
+++ fullStackProject1/src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path:'',
-    loadChildren: () => import('./management/management.module').then(m => m.ManagementModule)
-  }
+  
 ];
 
 @NgModule({
